71f6805034616ee539ab8851e1f0f12a08c38ef9
Belte52 <149451235+Belte52@users.noreply.github.com>
2024-07-19 19:18:58 -0700

    push

---
+++ CS110-6-Lab1.py
@@ -0,0 +1,19 @@
+### Daniel Ortega
+###CS110 summer2024 week 6
+
+
+original_price = input('original_price ')
+rate = input('rate ')
+   
+print("original_price = " + original_price)
+print("rate = " + rate)
+op = float(original_price)
+r = float(rate)
+
+float = da = 0.0
+float = final_price = 0.0
+float = fp = 0.0
+da = op * r / 100
+fp = op - da
+print('final_price', fp)
+print('discount_amount', da)
