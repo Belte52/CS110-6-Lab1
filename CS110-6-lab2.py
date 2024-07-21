ae9a1f5fa875041c66803b27c5b6c51c804e4472
Belte52 <149451235+Belte52@users.noreply.github.com>
2024-07-20 18:22:27 -0700

    hopeful

--- CS110-6-lab2.py
+++ CS110-6-lab2.py
@@ -1,37 +1,30 @@
 ###Daniel Ortega
 ###CS110-week6-lab2
+##Calculate Wages
 
-#workhours = input('workhours')
-reg_wage = input('reg_wage')
-#reg_hours = input('reg_hours')
-#reg_rate = input('reg_rate')
-#ov_rate = input('ov_rate')
-ov_wage = input('ov_wage')
-#overtime = input('overtime')
-print("reg_wage = " + reg_wage)
-print("ov_wage = " + ov_wage)
+reg_hours = 40
+reg_rate = 18.25
+ov_rate = 27.78
 
-OT = float('overtime')
-WH = float('workhours')
-RH = float('reg_hours')
-RR = float('reg_rate')
-OTR= float('ov_rate')
-RW = float('reg_wage')
-OTW= float('ov_wage')
+float = Regular_wage = 730.0
+float = OT_wage = 0.0
+float = Total_wage = 0.0
+workhours = int(input("Hours Worked?"))
+OT_hours = workhours - reg_hours
+print("OT_hours:", OT_hours)
 
-float = RH = 40
-float = OT = 0.0
-float = OTW  = 0.0
-float = OTR = 27.78
-float = WH = 50.
-float = RW = 730.0
-float = RR = 18.25
+Regular_Wage = reg_hours * reg_rate
+print("Regular_Wage is:", Regular_Wage)
 
-OT =  WH - RH
-OTW = OT * OTR
-RW = RH * RR
-total_wage = RW + OTW
 
-#print('reg_wage')
-#print('ov_wage')
-print('total_wage')
+OT_hours = workhours - 40
+OT_wage = OT_hours * ov_rate
+print("OT_wage:", OT_wage)
+    
+
+Total_wage = OT_wage + Regular_wage
+print("Total_wage:", Total_wage)  
+
+
+
+
---
+++ ranfom.py
@@ -0,0 +1,47 @@
+def average(numbers):
+    if len(numbers) == 0:
+        return None
+    else:
+        total = 0
+        for num in numbers:
+            total += num
+        return total / len(numbers)
+list = [3, 5, 6, 8]
+print("The average of list is " , average(list))
+
+
+
+#workhours = input('workhours')
+reg_wage = input('reg_wage')
+#reg_hours = input('reg_hours')
+#reg_rate = input('reg_rate')
+#ov_rate = input('ov_rate')
+ov_wage = input('ov_wage')
+#overtime = input('overtime')
+print("reg_wage = " + reg_wage)
+print("ov_wage = " + ov_wage)
+
+OT = float('overtime')
+WH = float('workhours')
+RH = float('reg_hours')
+RR = float('reg_rate')
+OTR= float('ov_rate')
+RW = float('reg_wage')
+OTW= float('ov_wage')
+
+float = RH = 40
+float = OT = 0.0
+float = OTW  = 0.0
+float = OTR = 27.78
+float = WH = 50.
+float = RW = 730.0
+float = RR = 18.25
+
+OT =  WH - RH
+OTW = OT * OTR
+RW = RH * RR
+total_wage = RW + OTW
+
+#print('reg_wage')
+#print('ov_wage')
+print('total_wage')
